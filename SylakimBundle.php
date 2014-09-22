adf0f9490cc986a9b521d7b340ec2ac2d3d90696
Romain Monceau <romain@akeneo.com>
2014-09-22 21:29:07 +0200

    Create SylakimBundle

---
+++ SylakimBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sylius\Bundle\SylakimBundle;
+
+/**
+ * Bundle connecting Sylius and Akeneo PIM
+ *
+ * @author    <AUTHOR>
+ * @copyright <COPYRIGHT>
+ * @license   http://opensource.org/licenses/MIT The MIT License
+ */
+class SylakimBundle extends Bundle
+{
+}
