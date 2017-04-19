6cc4b1da5ecaa197c817bbe4ffd0aa53830f69de
Kamil Kokot <kamil@kokot.me>
2017-04-19 09:43:02 +0200

    Provide boilerplate for Sylakim

---
+++ src/DependencyInjection/SylakeSylakimExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sylake\Sylakim\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class SylakeSylakimExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    }
+}
---
+++ src/SylakeSylakimBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sylake\Sylakim;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SylakeSylakimBundle extends Bundle
+{
+
+}
