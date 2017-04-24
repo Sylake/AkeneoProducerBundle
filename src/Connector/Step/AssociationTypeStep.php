a6743554320b10137c0fe5ed7e3ffb1c967b0240
Kamil Kokot <kamil@kokot.me>
2017-04-24 15:18:30 +0200

    Provide naive association type implementation

--- src/Connector/Step/AssociationTypeStep.php
+++ src/Connector/Step/AssociationTypeStep.php
@@ -6,6 +6,7 @@ use Akeneo\Component\Batch\Job\JobRepositoryInterface;
 use Akeneo\Component\Batch\Model\StepExecution;
 use Akeneo\Component\Batch\Step\AbstractStep;
 use Pim\Component\Catalog\Repository\AssociationTypeRepositoryInterface;
+use Sylake\Sylakim\Connector\Client\AssociationTypeClientFactoryInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 final class AssociationTypeStep extends AbstractStep
@@ -16,17 +17,24 @@ final class AssociationTypeStep extends AbstractStep
     private $associationTypeRepository;
 
     /**
+     * @var AssociationTypeClientFactoryInterface
+     */
+    private $associationTypeClientFactory;
+
+    /**
      * {@inheritdoc}
      */
     public function __construct(
         $name,
         EventDispatcherInterface $eventDispatcher,
         JobRepositoryInterface $jobRepository,
-        AssociationTypeRepositoryInterface $associationTypeRepository
+        AssociationTypeRepositoryInterface $associationTypeRepository,
+        AssociationTypeClientFactoryInterface $associationTypeClientFactory
     ) {
         parent::__construct($name, $eventDispatcher, $jobRepository);
 
         $this->associationTypeRepository = $associationTypeRepository;
+        $this->associationTypeClientFactory = $associationTypeClientFactory;
     }
 
     /**
@@ -34,6 +42,23 @@ final class AssociationTypeStep extends AbstractStep
      */
     protected function doExecute(StepExecution $stepExecution)
     {
-        
+        $jobParameters = $stepExecution->getJobParameters();
+        $associationTypeClient = $this->associationTypeClientFactory->create(
+            $jobParameters['url'],
+            $jobParameters['public_id'],
+            $jobParameters['secret']
+        );
+
+        $associationTypes = $this->associationTypeRepository->findAll();
+        foreach ($associationTypes as $associationType) {
+            if ($associationTypeClient->exists($associationType)) {
+                $associationTypeClient->update($associationType);
+
+                return;
+            }
+
+            $associationTypeClient->create($associationType);
+        }
+
     }
 }
