a444e2df207b2a7abcc18d7ad52ab05acb706e0f
Kamil Kokot <kamil@kokot.me>
2017-05-16 18:39:02 +0200

    Delete config file in favour of prepending RabbitMQ bundle configuration

--- src/DependencyInjection/SylakeAkeneoProducerExtension.php
+++ src/DependencyInjection/SylakeAkeneoProducerExtension.php
@@ -4,10 +4,11 @@ namespace Sylake\AkeneoProducerBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class SylakeAkeneoProducerExtension extends Extension
+final class SylakeAkeneoProducerExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -18,5 +19,26 @@ final class SylakeAkeneoProducerExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.xml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        if (!$container->hasExtension('old_sound_rabbit_mq')) {
+            throw new \RuntimeException('Make sure OldSoundRabbitMqBundle is enabled.');
+        }
+
+        $container->prependExtensionConfig('old_sound_rabbit_mq', [
+            'producers' => [
+                'akeneo' => [
+                    'exchange_options' => [
+                        'name' => 'akeneo',
+                        'type' => 'fanout',
+                    ],
+                ],
+            ],
+        ]);
     }
 }
