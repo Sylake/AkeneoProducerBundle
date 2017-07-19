d7031fab8912e67001fcff46330982ec67d861ca
Rokas Mikalkėnas <rokas.mikalkenas@nfq.lt>
2017-07-19 11:59:20 +0300

    Priduced categories cache added to CategoryTranslationUpdatedListener.

--- src/Listener/CategoryTranslationUpdatedListener.php
+++ src/Listener/CategoryTranslationUpdatedListener.php
@@ -11,9 +11,17 @@ final class CategoryTranslationUpdatedListener
     /** @var ItemProjectorInterface */
     private $categoryProjector;
 
+    /** @var CategoryInterface[] */
+    private static $producedObjects = [];
+
     public function __construct(ItemProjectorInterface $categoryProjector)
     {
         $this->categoryProjector = $categoryProjector;
+    }
+
+    public function postUpdate(LifecycleEventArgs $event)
+    {
+        $this($event);
     }
 
     public function __invoke(LifecycleEventArgs $event)
@@ -26,6 +34,13 @@ final class CategoryTranslationUpdatedListener
 
         /** @var CategoryInterface $category */
         $category = $categoryTranslation->getForeignKey();
+
+        // All translations are persisted to category, no need to produce it multiple times
+        if (in_array($category, self::$producedObjects, true)) {
+            return;
+        }
+
+        self::$producedObjects[] = $category;
 
         $this->categoryProjector->__invoke($category);
     }
