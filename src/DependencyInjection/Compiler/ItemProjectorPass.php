eb56fda6ec1d37c60c4b5d8815636f1a1b9d0fc7
Kamil Kokot <kamil@kokot.me>
2017-07-21 14:06:52 +0200

    Add tests for defining item projectors as tagged services

---
+++ src/DependencyInjection/Compiler/ItemProjectorPass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sylake\AkeneoProducerBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class ItemProjectorPass implements CompilerPassInterface
+{
+    /** {@inheritdoc} */
+    public function process(ContainerBuilder $container)
+    {
+
+    }
+}
